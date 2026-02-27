73ab78b880a21bfdaf058df7d95058f8c9dea2dd
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-02-27 18:55:35 +0100

    Commands

---
+++ fastkit_cli/commands/__init__.py
@@ -0,0 +1,13 @@
+import fastkit_cli.commands.seed
+import fastkit_cli.commands.new
+import fastkit_cli.commands.make
+import fastkit_cli.commands.migrate
+import fastkit_cli.commands.server
+
+__all__ = [
+    'seed',
+    'new',
+    'make',
+    'migrate',
+    'server'
+]
